2620ece597185d9d69fbf7d7f1c7fb63dff8af6a
MastaWu <wu.stephend@gmail.com>
2015-04-11 15:06:22 -0400

    commit picture files

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
-//NewDayNature Router.js
+//NewDayNature Router.
+
+var multer = require('multer');
 
 module.exports = function(app, router, mysql){
 
@@ -7,7 +9,7 @@ module.exports = function(app, router, mysql){
         host: 'box1077.bluehost.com',
         user: 'stephgv0_admin',
         password: '123abc',
-        database: 'stephgv0_hearmenow'
+        database: 'stephgv0_newdaynature'
     });
 
     router.use(function (req, res, next) {
@@ -15,6 +17,27 @@ module.exports = function(app, router, mysql){
         next();
     });
 
+    app.get('/', function(req, res){
+
+        res.sendfile('index.html');
+
+    });
+
+    app.use(multer({dest: './pictures/',
+        rename: function (fieldname, filename) {
+            return filename + Date.now();
+        },
+        onFileUploadStart: function (file) {
+            console.log(file.originalname + ' is starting ...')
+        },
+        onFileUploadComplete: function (file) {
+            console.log(file.fieldname + ' uploaded to  ' + file.path)
+            db.query('INSERT INTO img_location(id, img_url, favs) VALUES (' + ', ' + file.path + ', favs)', function(err){
+                if(err) throw err;
+                console.log(file.path);
+            } )
+        }
+    }));
 
     app.get('/api/', function (req, res) {
 
@@ -29,5 +52,23 @@ module.exports = function(app, router, mysql){
         });
 
     });
+
+    app.get('/pictures/:id', function(req, res){
+
+        var picUrl = req.params.id;
+        console.log(picUrl);
+        res.sendfile('/pictures/'+ picUrl);
+
+    });
+
+
+
+    app.post('/upload', function(req, res){
+
+        res.send(req.files);
+
+    })
+
+    app.post('/local');
 
 };
